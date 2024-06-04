36825ddd8dff78a2c7d7c8601affe8375a36a44a
BadKarma99 <saggar.ayush@gmail.com>
2024-06-05 03:06:43 +0530

    chip

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,6 +5,7 @@ export default function SearchBar({ data }) {
   const [isLoading, setIsLoading] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [chips, setChips] = useState([]);
 
   const search = (value) => {
     setIsLoading(true);
@@ -24,6 +25,17 @@ export default function SearchBar({ data }) {
     }
   };
 
+  const handleChipClick = (value) => {
+    if (!chips.includes(value)) {
+      setChips([...chips, value]); 
+    }
+    setSearchValue(""); 
+  };
+
+  const handleRemoveChip = (chipToRemove) => {
+    setChips(chips.filter(chip => chip !== chipToRemove)); 
+  };
+
   return (
     <div className="fixed grid justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <label className="text-[rgba(122,122,123,255)] font-extralight text-xs">INPUT TAGS</label>
@@ -33,19 +45,30 @@ export default function SearchBar({ data }) {
           onChange={(e) => handleChange(e.target.value)} 
           value={searchValue}
           className="rounded-2xl bg-search-rgba p-2 w-80"
-          placeholder="Search..."
+          placeholder={chips.length > 0 ? "" : "Search..."}
         />
+        <div className="absolute top-1 -left-1 flex   items-center">
+          {chips.map((chip, index) => (
+            <div key={index} className="bg-white rounded-xl px-3 py-1 text-sm ml-2 shadow-xl ">
+              {chip}
+              <span className="ml-2 cursor-pointer text-sm" onClick={() => handleRemoveChip(chip)}>X</span>
+            </div>
+          ))}
+        </div>
         {isLoading && (
           <div className="absolute right-3 top-3">
             <ClipLoader size={20} color={"#123abc"} loading={isLoading} />
           </div>
         )}
       </div>
-      <div className="transition shadow-2xl shadow-black rounded-2xl  w-36 ">
+      <div className="transition shadow-xl shadow-black rounded-2xl  w-36 ">
         {filteredData.length > 0 ? (
           filteredData.map((value, key) => (
-            <div key={key} className="p-2 hover:shadow-inner transition bg-white
-             border-gray-300 cursor-pointer hover:bg-search-rgba hover:rounded-2xl ">
+            <div 
+              key={key} 
+              className="p-2 hover:shadow-inner transition bg-white border-gray-300 cursor-pointer hover:bg-search-rgba hover:rounded-2xl"
+              onClick={() => handleChipClick(value)} 
+            >
               {value}
             </div>
           ))
@@ -55,7 +78,6 @@ export default function SearchBar({ data }) {
           )
         )}
       </div>
-      
     </div>
   );
 }
