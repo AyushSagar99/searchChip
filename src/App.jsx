dd6ce1f9bfddeb2db67d89940a6260fc47a1aa9a
BadKarma99 <saggar.ayush@gmail.com>
2024-06-06 00:34:32 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import { data } from "./utils/data";
+import {motion} from "framer-motion"
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  
+  const [showMessage, setShowMessage] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
 
   return (
     <>
-    <Toaster position="top-center"/>
-      <div className="bg-back-rgba h-screen">
-        <SearchBar data={data}/>
+      <Toaster position="top-center" />
+      <div className="bg-back-rgba h-screen flex justify-center items-center">
+        {showMessage ? (
+          <motion.div className="text-center text-lg mb-4"
+          initial={{opacity:0,y:2000}}
+          animate={{opacity:1,y:0}}
+          transition={{delay:0.2}}
+          >Hi, I am your auto complete suggestions</motion.div>
+        ) : (
+          <SearchBar data={data} />
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -51,58 +51,63 @@ export default function SearchBar({ data }) {
   };
 
   return (
-    <div className="fixed grid justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[40vw]">
-      <label className="text-[rgba(122,122,123,255)] font-extralight text-xs mb-2">INPUT TAGS</label>
-      <div className="relative flex items-center rounded-2xl bg-search-rgba p-2 w-full">
-        <div className="flex flex-wrap items-center flex-grow">
-          {chips.map((chip, index) => (
-            <div key={index} className="bg-white rounded-lg px-3 py-1 flex leading-none shadow-gray-700 text-sm mr-2 mb-1 shadow-sm">
-              {chip}
-              <span className="ml-2 cursor-pointer text-md font-medium text-center text-stone-500 rotate-45" onClick={() => handleRemoveChip(chip)}> <MdAdd /> </span>
-            </div>
-          ))}
-          <input 
-            type="search" 
-            onChange={(e) => handleChange(e.target.value)} 
-            value={searchValue}
-            ref={inputRef}
-            autoFocus
-            className="bg-search-rgba p-2 ml-2 flex-grow focus:outline-none "
-            placeholder={chips.length > 0 ? "" : "Search..."}
-            style={{ minWidth: '100px' }}
-          />
-        </div>
-        {isLoading && (
-          <div className="absolute right-3 top-3">
-            <ClipLoader size={20} color={"#123abc"} loading={isLoading} />
-          </div>
-        )}
-      </div>
-      <div className="transition shadow-xl shadow-black rounded-2xl mt-2 w-full bg-white">
-        {filteredData.length > 0 && searchValue !== "" ? (
-          filteredData.map((value, key) => {
-            const startIndex = value.toLowerCase().indexOf(searchValue.toLowerCase());
-            const endIndex = startIndex + searchValue.length;
-            const matchedText = value.slice(startIndex, endIndex);
-            const beforeText = value.slice(0, startIndex);
-            const afterText = value.slice(endIndex);
-
-            return (
-              <div 
-                key={key} 
-                className="p-2 hover:shadow-inner transition bg-white border-gray-300 cursor-pointer
-                 hover:bg-search-rgba hover:rounded-2xl rounded-xl"
-                onClick={() => handleChipClick(value)} 
-              >
-                {beforeText}<span className="text-blue-500">{matchedText}</span>{afterText}
+    <div className="fixed inset-0 flex flex-col justify-between items-center">
+      <div className="flex flex-col justify-center items-center w-full max-w-lg p-4 mt-auto mb-auto">
+        <label className="text-[rgba(122,122,123,255)] font-extralight text-xs mb-2">INPUT TAGS</label>
+        <div className="relative flex items-center rounded-2xl bg-search-rgba p-2 w-full">
+          <div className="flex flex-wrap items-center flex-grow">
+            {chips.map((chip, index) => (
+              <div key={index} className="bg-white rounded-lg px-3 py-1 flex leading-none shadow-gray-700 text-sm mr-2 mb-1 shadow-sm">
+                {chip}
+                <span className="ml-2 cursor-pointer text-md font-medium text-center text-stone-500 rotate-45" onClick={() => handleRemoveChip(chip)}> <MdAdd /> </span>
               </div>
-            );
-          })
-        ) : (
-          !isLoading && searchValue && (
-            <div className="p-2 text-gray-500 shadow-none">No matches found</div>
-          )
-        )}
+            ))}
+            <input 
+              type="search" 
+              onChange={(e) => handleChange(e.target.value)} 
+              value={searchValue}
+              ref={inputRef}
+              autoFocus
+              className="bg-search-rgba p-2 ml-2 flex-grow focus:outline-none"
+              placeholder={chips.length > 0 ? "" : "Search..."}
+              style={{ minWidth: '100px' }}
+            />
+          </div>
+          {isLoading && (
+            <div className="absolute right-3 top-3">
+              <ClipLoader size={20} color={"#123abc"} loading={isLoading} />
+            </div>
+          )}
+        </div>
+        <div className="transition shadow-xl shadow-black rounded-2xl mt-2 w-full bg-white">
+          {filteredData.length > 0 && searchValue !== "" ? (
+            filteredData.map((value, key) => {
+              const startIndex = value.toLowerCase().indexOf(searchValue.toLowerCase());
+              const endIndex = startIndex + searchValue.length;
+              const matchedText = value.slice(startIndex, endIndex);
+              const beforeText = value.slice(0, startIndex);
+              const afterText = value.slice(endIndex);
+
+              return (
+                <div 
+                  key={key} 
+                  className="p-2 hover:shadow-inner transition bg-white border-gray-300 cursor-pointer
+                   hover:bg-search-rgba hover:rounded-2xl rounded-xl"
+                  onClick={() => handleChipClick(value)} 
+                >
+                  {beforeText}<span className="text-blue-500">{matchedText}</span>{afterText}
+                </div>
+              );
+            })
+          ) : (
+            !isLoading && searchValue && (
+              <div className="p-2 text-gray-500 shadow-none">No matches found</div>
+            )
+          )}
+        </div>
+      </div>
+      <div className="text-center mb-4 p-4 w-full max-w-lg">
+        <p className="text-gray-500"><span className="text-black">Chips</span> can be utilized to showcase <span className="text-black">autocomplete suggestions</span> as users input information</p>
       </div>
     </div>
   );
